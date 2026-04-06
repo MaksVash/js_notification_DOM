9afbad513f09ed07209c5b7121e7695d6f83ab40
Maksim Vashuk <maximka.vashuk@gmail.com>
2026-04-06 23:11:28 +0200

    add notification

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,29 @@
 
 const pushNotification = (posTop, posRight, title, description, type) => {
   // write code here
+  const message = document.createElement('div');
+
+  message.classList.add('notification', type);
+
+  message.style.top = `${posTop}px`;
+  message.style.right = `${posRight}px`;
+
+  const titelElement = document.createElement('h2');
+
+  titelElement.classList.add('title');
+  titelElement.textContent = title;
+
+  const descriptionElement = document.createElement('p');
+
+  descriptionElement.textContent = description;
+
+  message.appendChild(titelElement);
+  message.appendChild(descriptionElement);
+  document.body.appendChild(message);
+
+  setTimeout(() => {
+    message.style.display = 'none';
+  }, 2000);
 };
 
 pushNotification(
